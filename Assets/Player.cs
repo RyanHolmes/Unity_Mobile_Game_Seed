5f1ab7277ac1f91c4cbbb371aa7383e65ec492fd
Ryan Holmes <ryankaz94@gmail.com>
2017-01-31 19:29:17 -0500

    touch manager, basic dragging action

---
+++ Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public Vector3 target;
+
+	void Start () {
+		target = this.transform.position;
+		this.GetComponent<Renderer> ().material.color = Color.white;
+	}
+	
+	void Update () {
+		this.transform.position = Vector3.Lerp (this.transform.position, target, Time.deltaTime * 7);
+	}
+
+	public void OnTouchDown(Vector3 point){
+		this.GetComponent<Renderer> ().material.color = Color.red;
+	}
+
+	public void OnTouchHold(Vector3 point){
+		this.GetComponent<Renderer> ().material.color = Color.red;
+		target = new Vector3 (point.x, this.transform.position.y, this.transform.position.z);
+	}
+
+	public void OnTouchUp(Vector3 point){
+		this.GetComponent<Renderer> ().material.color = Color.white;
+	}
+}
---
+++ Assets/TouchManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour {
+
+//	public LayerMask touchMask;
+	
+	void Update () {
+	// For testing in unity editor
+	#if UNITY_EDITOR
+		if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0)) {
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				RaycastHit hit;
+
+			if (Physics.Raycast (ray, out hit)) {
+				GameObject recipient = hit.transform.gameObject;
+
+				if (Input.GetMouseButtonDown(0)) {
+					recipient.SendMessage ("OnTouchDown", hit.point);
+				}
+				if (Input.GetMouseButtonUp(0)) {
+					recipient.SendMessage ("OnTouchUp", hit.point);
+				}
+				if (Input.GetMouseButton(0)) {
+					recipient.SendMessage ("OnTouchHold", hit.point);
+				}
+			}
+		}
+	#endif
+		// For mobile
+		if (Input.touchCount > 0) {
+			foreach (Touch touch in Input.touches) {
+				Ray ray = Camera.main.ScreenPointToRay (touch.position);
+				RaycastHit hit;
+
+				if (Physics.Raycast (ray, out hit)) {
+					GameObject recipient = hit.transform.gameObject;
+
+					if (touch.phase == TouchPhase.Began) {
+						recipient.SendMessage ("OnTouchDown", hit.point);
+					}
+					if (touch.phase == TouchPhase.Ended) {
+						recipient.SendMessage ("OnTouchUp", hit.point);
+					}
+					if (touch.phase == TouchPhase.Moved) {
+						recipient.SendMessage ("OnTouchHold", hit.point);
+					}
+					if (touch.phase == TouchPhase.Canceled) {
+						recipient.SendMessage ("OnTouchCancelled", SendMessageOptions.DontRequireReceiver);
+					}
+				}
+
+			}
+		}
+	}
+}
