59df08b5587c83503ec4ac88ae8f844ea75b1f3b
Ryan Holmes <ryankaz94@gmail.com>
2017-02-01 17:46:48 -0500

    docs and helper methods

--- Assets/Scripts/DraggableObject.cs
+++ Assets/Scripts/DraggableObject.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections;
+﻿// An example of how to use the TouchManager to drag an object via touch screen
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class DraggableObject : MonoBehaviour {
 
+	//Where we are dragging to
 	public Vector3 target;
 
 	void Start () {
+		
 		target = this.transform.position;
 		this.GetComponent<Renderer> ().material.color = Color.white;
 	}
@@ -21,7 +24,7 @@ public class Player : MonoBehaviour {
 
 	public void OnTouchHold(Vector3 point){
 		this.GetComponent<Renderer> ().material.color = Color.red;
-		target = new Vector3 (point.x, this.transform.position.y, this.transform.position.z);
+		target = new Vector3 (point.x, point.y, this.transform.position.z);
 	}
 
 	public void OnTouchUp(Vector3 point){
