b53e7d6b1941f08f60a5eb91cfdacf59c9995e6a
Ryan Holmes <ryankaz94@gmail.com>
2017-02-13 22:54:32 -0500

    added logo and load scene

---
+++ Assets/Scripts/Load.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Load : MonoBehaviour {
+
+	public float loadTime = 5;
+	private float currentTime;
+
+	// Use this for initialization
+	void Start () {
+		currentTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time - currentTime >= loadTime){
+			SceneManager.LoadScene ("MenuScene");
+		}
+	}
+}
