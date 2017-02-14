a80adc9283377a933f4f6664231e2ed49345ef8c
Ryan Holmes <ryankaz94@gmail.com>
2017-02-13 19:30:40 -0500

    scene fading

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour {
+
+	public void ClickPlay(){
+		SceneManager.LoadScene ("GameScene");
+	}
+
+//	public IEnumerator FadeToNextLevel() {
+//		float fadetime = Camera.main.GetComponent<SceneFade>().BeginFade(-1);
+//		yield return new WaitForSeconds(fadetime);
+//		SceneManager.LoadScene("GameScene");
+//	}
+//
+//	public void ClickPlay() {
+//		StartCoroutine(FadeToNextLevel());
+//	}
+}
